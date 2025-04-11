176c37b1d563f3fb23fc187362b44179956b3302
Bruno Romano <brunoromano@gmail.com>
2025-04-11 00:52:41 -0300

    WIP: building service collector form

--- src/app/service-collector/service-collector.component.ts
+++ src/app/service-collector/service-collector.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-service-collector',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './service-collector.component.html',
   styleUrl: './service-collector.component.css'
 })
 export class ServiceCollectorComponent {
+  serviceCollectorForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { 
+    this.serviceCollectorForm = this.fb.nonNullable.group({
+      serviceName: ['', [Validators.required]]
+    });
+  }
+  
+  onSubmit() {
+    if (this.serviceCollectorForm.valid) {
+      console.log('Dados do Formulário:', this.serviceCollectorForm.value);
+    }
+  }
 
 }
