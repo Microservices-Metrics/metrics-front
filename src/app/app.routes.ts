1619076dabc6e5d96def8e6cfb79ff931d9a9dc4
Bruno Romano <brunopromano@gmail.com>
2025-10-21 01:11:01 -0300

    Add Collections component and update routing for collections page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { MetricsComponent } from './metrics/metrics.component';
 import { ServiceCollectorComponent } from './service-collector/service-collector.component';
 import { MicroservicesComponent } from './microservices/microservices.component';
+import { CollectionsComponent } from './collections/collections.component';
 
 export const routes: Routes = [
+    { path: 'collections', component: CollectionsComponent },
     { path: 'metrics', component: MetricsComponent },
     { path: 'microservices', component: MicroservicesComponent },
     { path: 'metric-collectors', component: ServiceCollectorComponent }
